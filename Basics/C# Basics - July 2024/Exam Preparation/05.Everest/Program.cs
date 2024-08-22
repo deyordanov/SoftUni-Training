8fdaf70925c9cce2d9a74afd7b01c869ec52e974
Denis Yordanov <deyordanov777@gmail.com>
2024-08-22 20:24:37 +0300

    Added a new exercise in 'Exam Preparation'.

---
+++ Basics/C# Basics - July 2024/Exam Preparation/05.Everest/Program.cs
@@ -0,0 +1,36 @@
+﻿const int baseHeight = 5364;
+const int peakHeight = 8848;
+
+// We start from '1' since up until now ( for the first day ) we accept Atanas has climbed up to the height of the camp - 5364 meters
+int daysClimbed = 1;
+int totalMetersClimbed = baseHeight;
+bool isPeakReached = false;
+string command;
+while((command = Console.ReadLine()) != "END" && daysClimbed < 5)
+{
+    string sleepOrClimb = command;
+    if (command == "Yes")
+    {
+        daysClimbed++;
+    }
+
+    int metersClimbed = int.Parse(Console.ReadLine());
+
+    totalMetersClimbed += metersClimbed;
+
+    if (totalMetersClimbed >= peakHeight)
+    {
+        isPeakReached = true;
+        break; // The 'return' keyword can be used here! This way we wouldn't need the boolean variable 'isPeakReached'.
+    }
+}
+
+if (isPeakReached)
+{
+    Console.WriteLine($"Goal reached for {daysClimbed} days!");
+}
+else
+{
+    Console.WriteLine("Failed!");
+    Console.WriteLine(totalMetersClimbed);
+}
