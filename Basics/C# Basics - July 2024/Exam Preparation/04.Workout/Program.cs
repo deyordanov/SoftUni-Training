bda4ba2436a8c53854a5782750e7a0e155d1fb5b
Denis Yordanov <deyordanov777@gmail.com>
2024-08-22 19:57:08 +0300

    Added a new exercise in 'Exam Preparation'.

---
+++ Basics/C# Basics - July 2024/Exam Preparation/04.Workout/Program.cs
@@ -0,0 +1,21 @@
+﻿int days = int.Parse(Console.ReadLine());
+double km = double.Parse(Console.ReadLine());
+
+double totalKm = km;
+for (int i = 1; i <= days; i++)
+{
+    double percentage = double.Parse(Console.ReadLine());
+
+    km += km * (percentage / 100);
+
+    totalKm += km;
+}
+
+if (totalKm >= 1000)
+{
+    Console.WriteLine($"You've done a great job running {Math.Ceiling(totalKm - 1000)} more kilometers!");
+}
+else
+{
+    Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {Math.Ceiling(1000 - totalKm)} more kilometers");
+}
