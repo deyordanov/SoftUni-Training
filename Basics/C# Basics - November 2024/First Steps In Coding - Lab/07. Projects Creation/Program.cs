dbc21ca996470b3592efe7df93de637c9aebc2ec
Denis Yordanov <deyordanov777@gmail.com>
2024-11-01 22:53:27 +0200

    Added the solutions for all exercises in "First Steps In Coding - Lab".

---
+++ Basics/C# Basics - November 2024/First Steps In Coding - Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = Console.ReadLine();
+int projects = int.Parse(Console.ReadLine());
+
+int hoursToCompleteProjects = projects * 3;
+
+Console.WriteLine($"The architect {name} will need {hoursToCompleteProjects} hours to complete {projects} project/s.");
