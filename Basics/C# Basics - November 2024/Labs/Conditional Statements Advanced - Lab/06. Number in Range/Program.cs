1a5d54d2b85b3914313d0a8f40bd5ed06a69ef0b
Denis Yordanov <deyordanov777@gmail.com>
2024-11-16 07:58:17 +0200

    Added the solutions for all exercises in "Conditional Statements Advanced - Lab".

---
+++ Basics/C# Basics - November 2024/Labs/Conditional Statements Advanced - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+if (number >= -100 && number <= 100 && number != 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+//if (number >= - 100)
+//{
+//    if (number <= 100)
+//    {
+//        if (number != 0)
+//        {
+//            Console.WriteLine("Yes");
+//        }
+//        else
+//        {
+//            Console.WriteLine("No");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("No");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
