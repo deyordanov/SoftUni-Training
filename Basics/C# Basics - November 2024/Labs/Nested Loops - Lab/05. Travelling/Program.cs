929720b6833372c5e1c8bd394d9e1aeb62fe5150
Denis Yordanov <deyordanov777@gmail.com>
2024-12-06 20:08:56 +0200

    Added the solutions for all exercises in "Nested Loops - Lab".

---
+++ Basics/C# Basics - November 2024/Labs/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,13 @@
+﻿string destination = Console.ReadLine();
+while (destination != "End")
+{
+    double moneyToSave = double.Parse(Console.ReadLine());
+    while (moneyToSave > 0)
+    {
+        double money = double.Parse(Console.ReadLine());
+        moneyToSave -= money;
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+    destination = Console.ReadLine();
+}
