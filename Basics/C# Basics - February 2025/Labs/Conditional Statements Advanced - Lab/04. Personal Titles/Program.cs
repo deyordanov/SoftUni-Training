77418be7dc9bd531f8d06e56655d5276b945408d
Denis Yordanov <deyordanov777@gmail.com>
2025-02-15 08:15:41 +0200

    Added the solutions for all exercises in "Conditional Statements Advanced - Lab".

---
+++ Basics/C# Basics - February 2025/Labs/Conditional Statements Advanced - Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,28 @@
+﻿double age = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+string title = "";
+if (age < 16)
+{
+    if (gender == 'm')
+    {
+        title = "Master";
+    }
+    else if (gender == 'f')
+    {
+        title = "Miss";
+    }
+}
+else if (age >= 16)
+{
+    if (gender == 'm')
+    {
+        title = "Mr.";
+    }
+    else if (gender == 'f')
+    {
+        title = "Ms.";
+    }
+}
+
+Console.WriteLine(title);
---
+++ Basics/C# Basics - February 2025/Labs/Conditional Statements Advanced - Lab/07. Working Hours/Program.cs
@@ -0,0 +1,18 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+bool isHourValid = hour >= 10 && hour <= 18;
+if (isHourValid && 
+    (day == "Monday" ||
+    day == "Tuesday" ||
+    day == "Wednesday" ||
+    day == "Thursday" ||
+    day == "Friday" ||
+    day == "Saturday"))
+{
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
